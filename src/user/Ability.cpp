e8994e49a229ab17f18d24876c2e58025d33e6c4
K-rin <kingukarin@gmail.com>
2022-09-24 11:28:16 +0900

    不必要なファイル削除

--- src/user/Ability.cpp
+++
@@ -1,8 +0,0 @@
-#include"Ability.h"
-#include"D3D12App.h"
-
-Ability::Ability(const std::string& TexName)
-{
-	static const std::string DIR = "resource/user/img/abilityIcon/";
-	m_iconTex = D3D12App::Instance()->GenerateTextureBuffer(DIR + TexName);
-}
