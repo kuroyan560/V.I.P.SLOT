9adc318739bc257722818b12bd5eccfb1475b868
K-rin <kingukarin@gmail.com>
2022-09-24 20:07:49 +0900

    プレイヤー横に等速移動

--- src/user/Player.cpp
+++ src/user/Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include"Object.h"
+#include"UsersInput.h"
 
 Player::Player()
 {
@@ -15,6 +16,37 @@ void Player::Init()
 
 void Player::Update()
 {
+//入力情報取得
+	const auto& input = *UsersInput::Instance();
+
+	//左スティック（移動）入力
+	const auto stickVec = input.GetLeftStickVec(0);
+
+	//Aボタン（ジャンプ）入力
+	const bool jumpTrigger = input.ControllerOnTrigger(0, XBOX_BUTTON::A);
+
+	//Xボタン（前方にショット）入力
+	const bool shotTrigger = input.ControllerOnTrigger(0, XBOX_BUTTON::X);
+
+	//LBボタン（BET）入力
+
+//入力情報を元に操作
+	//現在の座標取得
+	auto pos = m_modelObj->m_transform.GetPos();
+
+	//移動
+	const float MOVE_SPEED = 0.3f;
+	if (0.0f < stickVec.x)
+	{
+		pos.x += MOVE_SPEED;
+	}
+	else if (stickVec.x < 0.0f)
+	{
+		pos.x -= MOVE_SPEED;
+	}
+
+	//操作後の座標を反映
+	m_modelObj->m_transform.SetPos(pos);
 }
 
 #include"DrawFunc3D.h"
