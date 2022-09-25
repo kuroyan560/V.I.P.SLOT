1e43b41510cab186bb040de7ca6a6a8142319269
K-rin <kingukarin@gmail.com>
2022-09-25 10:52:23 +0900

    リール管理

--- src/user/SlotMachine.cpp
+++ src/user/SlotMachine.cpp
@@ -1,22 +1,96 @@
 #include "SlotMachine.h"
 #include"Object.h"
+#include"Model.h"
 
 SlotMachine::SlotMachine()
 {
 	//スロットマシン生成
 	m_slotMachineObj = std::make_shared<ModelObject>("resource/user/model/", "slotMachine.glb");
+
+	//モデルからリールの情報取得
+	for (auto& mesh : m_slotMachineObj->m_model->m_meshes)
+	{
+		//マテリアル名取得
+		const auto& materialName = mesh.material->name;
+
+		for (int reelIdx = 0; reelIdx < REEL::NUM; ++reelIdx)
+		{
+			//リールのメッシュ発見
+			if (REEL_MATERIAL_NAME[reelIdx].compare(materialName) == 0)
+			{
+				//リールのメッシュポインタをアタッチ
+				m_reels[reelIdx].Attach(&mesh);
+			}
+		}
+	}
 }
 
 void SlotMachine::Init()
 {
+	//リール初期化
+	for (int reelIdx = 0; reelIdx < REEL::NUM; ++reelIdx)m_reels[reelIdx].Init();
 }
 
 void SlotMachine::Update()
 {
+	const float REEL_SPIN_SPEED = -0.005f;
+
+	//リール回転
+	for (int reelIdx = 0; reelIdx < REEL::NUM; ++reelIdx)
+	{
+		//リール取得
+		auto& reel = m_reels[reelIdx];
+
+		//リール回転
+		reel.m_vOffset += REEL_SPIN_SPEED;
+
+		//リールメッシュに反映
+		reel.SpinAffectUV();
+	}
 }
 
 #include"DrawFunc3D.h"
 void SlotMachine::Draw(std::weak_ptr<LightManager> LigMgr, std::weak_ptr<Camera> Cam)
 {
 	DrawFunc3D::DrawNonShadingModel(m_slotMachineObj, *Cam.lock(), 1.0f, AlphaBlendMode_None);
+}
+
+void SlotMachine::Reel::Attach(ModelMesh* ReelMesh)
+{
+	m_meshPtr = ReelMesh;
+
+	//リールメッシュ頂点のV初期化値保存
+	auto& vertices = m_meshPtr->mesh->vertices;
+	m_initV.resize(vertices.size());
+	for (int vertIdx = 0; vertIdx < static_cast<int>(vertices.size()); ++vertIdx)
+	{
+		m_initV[vertIdx] = vertices[vertIdx].uv.y;
+	}
+}
+
+void SlotMachine::Reel::Init(std::shared_ptr<TextureBuffer> ReelTex)
+{
+	//リールのメッシュが見つけられていない
+	if (m_meshPtr == nullptr)
+	{
+		printf("The reel hasn't found its mesh pointer.It is nullptr.");
+		assert(0);
+	}
+
+	//リールのテクスチャ指定
+	if (ReelTex)m_meshPtr->material->texBuff[COLOR_TEX] = ReelTex;
+	//回転量リセット
+	m_vOffset = 0.0f;
+	//リールメッシュに反映
+	SpinAffectUV();
+}
+
+void SlotMachine::Reel::SpinAffectUV()
+{
+	//頂点情報のUV回転量セット
+	for (int vertIdx = 0; vertIdx < static_cast<int>(m_meshPtr->mesh->vertices.size()); ++vertIdx)
+	{
+		m_meshPtr->mesh->vertices[vertIdx].uv.y = m_initV[vertIdx] + m_vOffset;
+	}
+	m_meshPtr->mesh->Mapping();
 }
--- src/user/SlotMachine.h
+++ src/user/SlotMachine.h
@@ -1,13 +1,44 @@
 #pragma once
 #include<memory>
+#include<array>
+#include<vector>
+#include<string>
 class ModelObject;
 class LightManager;
 class Camera;
+class TextureBuffer;
+class ModelMesh;
 
 class SlotMachine
 {
 	//スロットマシン
 	std::shared_ptr<ModelObject>m_slotMachineObj;
+
+	//リール
+	enum REEL { LEFT, CENTER, RIGHT, NUM };
+	const std::array<std::string, REEL::NUM>REEL_MATERIAL_NAME =
+	{
+		"Reel_Left","Reel_Center","Reel_Right"
+	};
+	class Reel
+	{
+	private:
+		//対象のメッシュポインタ
+		ModelMesh* m_meshPtr = nullptr;
+		//UV値の初期化値
+		std::vector<float>m_initV;
+
+	public:
+		//回転量（UV値のV）
+		float m_vOffset = 0.0f;
+		//リールメッシュアタッチ
+		void Attach(ModelMesh* ReelMesh);
+		//初期化
+		void Init(std::shared_ptr<TextureBuffer>ReelTex = nullptr);
+		//回転をUV値に反映
+		void SpinAffectUV();
+	};
+	std::array<Reel, REEL::NUM>m_reels;
 	
 public:
 	SlotMachine();
