9ac75b14d05a12a3b3d2a524eebe5ffbd89f710e
K-rin <kingukarin@gmail.com>
2022-11-02 15:46:26 +0900

    手ブレでTimerを使わない。タイマーリセット時に大きく動いてしまうため

--- src/user/HandShake.cpp
+++ src/user/HandShake.cpp
@@ -4,22 +4,17 @@
 
 void HandShake::Init()
 {
-	m_timer.Reset(m_interval);
+	m_t = 0.0f;
 }
 
 void HandShake::Update(float arg_timeScale,const Matrix& arg_rotate)
 {
-	if (m_timer.UpdateTimer(arg_timeScale))
-	{
-		m_timer.Reset(m_interval);
-		m_seed.x = KuroFunc::GetRand(SEED_MIN, SEED_MAX);
-		m_seed.y = KuroFunc::GetRand(SEED_MIN, SEED_MAX);
-	}
+	m_t += 1.0f / static_cast<float>(m_interval);
 
 	//ŽèƒuƒŒ
 	Vec3<float>handShake = { 1.0f,1.0f,0.0f };
-	handShake.x = PerlinNoise::GetRand(m_timer.GetTimeRate(), 0.0f, m_seed.x) * m_val.x;
-	handShake.y = PerlinNoise::GetRand(0.0f, m_timer.GetTimeRate(), m_seed.y) * m_val.y;
+	handShake.x = PerlinNoise::GetRand(m_t, 0.0f, m_seed.x) * m_val.x;
+	handShake.y = PerlinNoise::GetRand(0.0f, m_t, m_seed.y) * m_val.y;
 
 	m_offset = KuroMath::TransformVec3(handShake, arg_rotate);
 }
--- src/user/HandShake.h
+++ src/user/HandShake.h
@@ -1,6 +1,5 @@
 #pragma once
 #include"Vec.h"
-#include"Timer.h"
 #include"KuroMath.h"
 
 //ŽèƒuƒŒ
@@ -9,7 +8,7 @@ class HandShake
 	static const int SEED_MIN = 0;
 	static const int SEED_MAX = 100;
 	Vec3<float>m_offset = { 0,0,0 };
-	Timer m_timer;
+	float m_t;
 	Vec2<int>m_seed = { 0,1 };
 
 public:
@@ -21,6 +20,6 @@ public:
 	void Update(float arg_timeScale,const Matrix& arg_rotate);
 
 	const Vec3<float>& GetOffset()const { return m_offset; }
-	const float& GetTimeRate()const { return m_timer.GetTimeRate(); }
+	const float& GetTimeRate()const { return m_t - static_cast<float>(static_cast<int>(m_t)); }
 };
 
