d877898714600582dd354eeec623ee27b7b85054
K-rin <kingukarin@gmail.com>
2022-12-22 12:25:44 +0900

    ガウシアンブラークラス引数の改名

--- src/engine/GaussianBlur.cpp
+++ src/engine/GaussianBlur.cpp
@@ -2,7 +2,7 @@
 #include"KuroEngine.h"
 #include"DrawFunc2D.h"
 
-void GaussianBlur::GeneratePipeline(std::array<std::shared_ptr<GraphicsPipeline>, PROCESS_NUM - 1>& DestPipeline, const DXGI_FORMAT& Format)
+void GaussianBlur::GeneratePipeline(std::array<std::shared_ptr<GraphicsPipeline>, PROCESS_NUM - 1>& arg_destPipeline, const DXGI_FORMAT& arg_format)
 {
     PipelineInitializeOption option(D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE, D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
     option.m_depthWriteMask = false;
@@ -17,7 +17,7 @@ void GaussianBlur::GeneratePipeline(std::array<std::shared_ptr<GraphicsPipeline>
 
     std::array<std::string, PROCESS_NUM - 1>vsEntoryPoint = { "HorizontalMain","VerticalMain" };
 
-    std::vector<RenderTargetInfo>renderTarget = { RenderTargetInfo(Format,AlphaBlendMode_None) };
+    std::vector<RenderTargetInfo>renderTarget = { RenderTargetInfo(arg_format,AlphaBlendMode_None) };
 
     auto sampler = WrappedSampler(false, true);
 
@@ -28,7 +28,7 @@ void GaussianBlur::GeneratePipeline(std::array<std::shared_ptr<GraphicsPipeline>
         shaders.m_vs = D3D12App::Instance()->CompileShader("resource/engine/GaussianBlur.hlsl", vsEntoryPoint[processIdx], "vs_6_4");
         shaders.m_ps = D3D12App::Instance()->CompileShader("resource/engine/GaussianBlur.hlsl", "PSmain", "ps_6_4");
 
-        DestPipeline[processIdx] = D3D12App::Instance()->GenerateGraphicsPipeline(
+        arg_destPipeline[processIdx] = D3D12App::Instance()->GenerateGraphicsPipeline(
             option, 
             shaders,
             SpriteMesh::Vertex::GetInputLayout(),
@@ -39,30 +39,30 @@ void GaussianBlur::GeneratePipeline(std::array<std::shared_ptr<GraphicsPipeline>
     }
 }
 
-GaussianBlur::GaussianBlur(const Vec2<int>& Size, const DXGI_FORMAT& Format, const float& BlurPower)
+GaussianBlur::GaussianBlur(const Vec2<int>& arg_size, const DXGI_FORMAT& arg_format, const float& arg_blurPower)
 {
-    GeneratePipeline(m_gPipeline, Format);
+    GeneratePipeline(m_gPipeline, arg_format);
 
     //重みテーブル定数バッファ
     m_weightConstBuff = D3D12App::Instance()->GenerateConstantBuffer(sizeof(float), s_weightNum, nullptr, "GaussianBlur - weight");
-    SetBlurPower(BlurPower);
+    SetBlurPower(arg_blurPower);
 
     //縦横ブラーの結果描画先
-    Vec2<int> xBlurTexSize = { Size.x / 2 , Size.y };
-    m_blurResult[X_BLUR] = D3D12App::Instance()->GenerateRenderTarget(Format, Color(0, 0, 0, 0), xBlurTexSize, L"GaussianBlur_HorizontalBlur_RenderTarget");
+    Vec2<int> xBlurTexSize = { arg_size.x / 2 , arg_size.y };
+    m_blurResult[X_BLUR] = D3D12App::Instance()->GenerateRenderTarget(arg_format, Color(0, 0, 0, 0), xBlurTexSize, L"GaussianBlur_HorizontalBlur_RenderTarget");
     m_spriteMeshes[X_BLUR] = std::make_unique<SpriteMesh>("GaussianBlur_HorizontalBlur");
-    m_spriteMeshes[X_BLUR]->SetSize(Size.Float());
+    m_spriteMeshes[X_BLUR]->SetSize(arg_size.Float());
 
-    Vec2<int> yBlurTexSize = { Size.x / 2 , Size.y / 2 };
-    m_blurResult[Y_BLUR] = D3D12App::Instance()->GenerateRenderTarget(Format, Color(0, 0, 0, 0), yBlurTexSize, L"GaussianBlur_VerticalBlur_RenderTarget");
+    Vec2<int> yBlurTexSize = { arg_size.x / 2 , arg_size.y / 2 };
+    m_blurResult[Y_BLUR] = D3D12App::Instance()->GenerateRenderTarget(arg_format, Color(0, 0, 0, 0), yBlurTexSize, L"GaussianBlur_VerticalBlur_RenderTarget");
     m_spriteMeshes[Y_BLUR] = std::make_unique<SpriteMesh>("GaussianBlur_VerticalBlur");
 
-    m_spriteMeshes[Y_BLUR]->SetSize(Size.Float());
+    m_spriteMeshes[Y_BLUR]->SetSize(arg_size.Float());
 
-    m_blurResult[FINAL] = D3D12App::Instance()->GenerateRenderTarget(Format, Color(0, 0, 0, 0), Size, L"GaussianBlur_Result_RenderTarget");
+    m_blurResult[FINAL] = D3D12App::Instance()->GenerateRenderTarget(arg_format, Color(0, 0, 0, 0), arg_size, L"GaussianBlur_Result_RenderTarget");
 }
 
-void GaussianBlur::SetBlurPower(const float& BlurPower)
+void GaussianBlur::SetBlurPower(const float& arg_blurPower)
 {
     // 重みの合計を記録する変数を定義する
     float total = 0;
@@ -71,7 +71,7 @@ void GaussianBlur::SetBlurPower(const float& BlurPower)
     // ループ変数のxが基準テクセルからの距離
     for (int x = 0; x < s_weightNum; x++)
     {
-        m_weights[x] = expf(-0.5f * (float)(x * x) / BlurPower);
+        m_weights[x] = expf(-0.5f * (float)(x * x) / arg_blurPower);
         total += 2.0f * m_weights[x];
     }
 
@@ -85,9 +85,9 @@ void GaussianBlur::SetBlurPower(const float& BlurPower)
 }
 
 #include"DrawFunc2D.h"
-void GaussianBlur::Register(const std::shared_ptr<TextureBuffer>& SourceTex)
+void GaussianBlur::Register(const std::shared_ptr<TextureBuffer>& arg_srcTex)
 {
-    const auto& sDesc = SourceTex->GetDesc();
+    const auto& sDesc = arg_srcTex->GetDesc();
     const auto& fDesc = m_blurResult[FINAL]->GetDesc();
     assert(sDesc.Width == fDesc.Width && sDesc.Height == fDesc.Height && sDesc.Format == fDesc.Format);
 
@@ -95,7 +95,7 @@ void GaussianBlur::Register(const std::shared_ptr<TextureBuffer>& SourceTex)
     {
         {KuroEngine::Instance()->GetParallelMatProjBuff(),CBV},
         {m_weightConstBuff,CBV},
-        {SourceTex,SRV},
+        {arg_srcTex,SRV},
     };
 
     for (auto& rt : m_blurResult)KuroEngine::Instance()->Graphics().ClearRenderTarget(rt);
@@ -120,7 +120,7 @@ void GaussianBlur::Register(const std::shared_ptr<TextureBuffer>& SourceTex)
 }
 
 #include"KuroEngine.h"
-void GaussianBlur::DrawResult(const AlphaBlendMode& AlphaBlend)
+void GaussianBlur::DrawResult(const AlphaBlendMode& arg_alphaBlend)
 {
-    DrawFunc2D::DrawExtendGraph2D({ 0,0 }, WinApp::Instance()->GetExpandWinSize(), m_blurResult[FINAL], 1.0f, AlphaBlend);
+    DrawFunc2D::DrawExtendGraph2D({ 0,0 }, WinApp::Instance()->GetExpandWinSize(), m_blurResult[FINAL], 1.0f, arg_alphaBlend);
 }
--- src/engine/GaussianBlur.h
+++ src/engine/GaussianBlur.h
@@ -9,7 +9,7 @@ class GaussianBlur
 private:
 	enum PROCESS { X_BLUR, Y_BLUR, FINAL, PROCESS_NUM };
 	static const int s_weightNum = 8;
-	static void GeneratePipeline(std::array<std::shared_ptr<GraphicsPipeline>, PROCESS_NUM - 1>& DestPipeline, const DXGI_FORMAT& Format);
+	static void GeneratePipeline(std::array<std::shared_ptr<GraphicsPipeline>, PROCESS_NUM - 1>& arg_destPipeline, const DXGI_FORMAT& arg_format);
 
 private:
 	//重みテーブル
@@ -29,15 +29,15 @@ private:
 
 public:
 	//ソース画像 & 結果描画先設定、ぼかし強さ
-	GaussianBlur(const Vec2<int>& Size, const DXGI_FORMAT& Format, const float& BlurPower = 8.0f);
+	GaussianBlur(const Vec2<int>& arg_size, const DXGI_FORMAT& arg_format, const float& arg_blurPower = 8.0f);
 	//ボケ具合
-	void SetBlurPower(const float& BlurPower);
+	void SetBlurPower(const float& arg_blurPower);
 	//グラフィックスマネージャに登録
-	void Register(const std::shared_ptr<TextureBuffer>& SourceTex);
+	void Register(const std::shared_ptr<TextureBuffer>& arg_srcTex);
 
 	//結果のテクスチャ取得
 	std::shared_ptr<RenderTarget>& GetResultTex() { return m_blurResult[FINAL]; }
 
 	//ウィンドウサイズで結果を描画
-	void DrawResult(const AlphaBlendMode& AlphaBlend);
+	void DrawResult(const AlphaBlendMode& arg_alphaBlend);
 };
